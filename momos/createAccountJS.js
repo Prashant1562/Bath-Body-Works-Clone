8795440e6b8a4ce612f7a1b2dca35eade50ca1f8
rampravesh9991 <rampravesh9991@gmail.com>
2022-06-18 15:04:13 +0530

    backuped all deleted files + additional files

---
+++ momos/createAccountJS.js
@@ -0,0 +1,31 @@
+
+let form = document.querySelector("form");
+let userData = JSON.parse(localStorage.getItem("userData")) || [];
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let data = {
+        name : form.name.value,
+        email : form.email.value,
+        password : form.password.value
+    }
+    console.log(data);
+    if(checkEmails(data.email)===true){
+        userData.push(data);
+        localStorage.setItem("userData",JSON.stringify(userData));
+        alert("Account created successfully");
+        window.location.href = "signupPage.html";
+    }else{
+        alert("Account already Exists");
+    }
+})
+
+function checkEmails(email){
+    let filtered = userData.filter(function(element){
+        return email === element.email;
+    })
+    if(filtered.length > 0){
+        return false;
+    }else{
+        return true;
+    }
+}
